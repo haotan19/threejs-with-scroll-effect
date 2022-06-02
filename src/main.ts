e59bef181c012a3aec157d044bec274af197a285
Hao Tan <80441177+haotan19@users.noreply.github.com>
2022-06-02 08:39:14 +0800

    Testing using wireframe

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { ScrollTrigger } from "gsap/all";
 import {
   BoxGeometry,
   Color,
+  Line,
+  LineBasicMaterial,
+  LineSegments,
   Mesh,
   MeshBasicMaterial,
   MeshPhongMaterial,
   PerspectiveCamera,
   Scene,
+  SphereGeometry,
   WebGLRenderer,
+  WireframeGeometry,
 } from "three";
 import { directionLight } from "./light";
 import "./style.css";
@@ -17,7 +22,7 @@ const CUBE_COLOR = ' #F5F5F5'
 
 gsap.registerPlugin(ScrollTrigger)
 
-const renderer = new WebGLRenderer({ alpha: true });
+const renderer = new WebGLRenderer({ alpha: true, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 const app = document.getElementById("app");
 
@@ -38,7 +43,20 @@ if (app) {
   const geometry = new BoxGeometry(10, 10, 10);
   const material = new MeshPhongMaterial({ color: new Color(CUBE_COLOR) , shininess: 30 });
   const cube = new Mesh(geometry, material);
-  scene.add(cube);
+  // scene.add(cube);
+
+  const sphereGeo = new SphereGeometry(10, 10, 10)
+  const sphereWireframe = new WireframeGeometry(sphereGeo)
+
+  const lineMaterial = new LineBasicMaterial({
+    color: new Color("#FF0000")
+  })
+  const sphereLine = new LineSegments(sphereWireframe, lineMaterial)
+//  sphereLine.material.depthTest = false;
+// sphereLine.material.opacity = 0.25;
+// sphereLine.material.transparent = true;
+
+scene.add(sphereLine)
 
   scene.add(directionLight(.1, 0, 0, 100))
   scene.add(directionLight(.9, 0, 80, 30))
