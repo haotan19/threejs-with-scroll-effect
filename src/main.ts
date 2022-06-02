cf0cd24fb36b5023936cc70361f41026837e4ffe
Hao Tan <80441177+haotan19@users.noreply.github.com>
2022-06-02 09:46:49 +0800

    Testing wireframe material

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,9 @@ import {
 import { directionLight } from "./light";
 import "./style.css";
 
-const CUBE_COLOR = ' #F5F5F5'
+const CUBE_COLOR = " #F5F5F5";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const renderer = new WebGLRenderer({ alpha: true, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -41,50 +41,73 @@ if (app) {
   const scene = new Scene();
 
   const geometry = new BoxGeometry(10, 10, 10);
-  const material = new MeshPhongMaterial({ color: new Color(CUBE_COLOR) , shininess: 30 });
+  const material = new MeshPhongMaterial({
+    color: new Color(CUBE_COLOR),
+    shininess: 30,
+  });
   const cube = new Mesh(geometry, material);
   // scene.add(cube);
 
-  const sphereGeo = new SphereGeometry(10, 10, 10)
-  const sphereWireframe = new WireframeGeometry(sphereGeo)
+  const sphereGeo = new SphereGeometry(10, 10, 10);
+  const sphereWireframe = new WireframeGeometry(sphereGeo);
 
   const lineMaterial = new LineBasicMaterial({
-    color: new Color("#FF0000")
+    color: new Color("#FF0000"),
+  });
+  const sphereLine = new LineSegments(sphereWireframe, lineMaterial);
+
+  gsap.to(sphereLine.rotation, {
+      duration: 100,
+      repeat: -1,
+      y: Math.PI * 1,
   })
-  const sphereLine = new LineSegments(sphereWireframe, lineMaterial)
-//  sphereLine.material.depthTest = false;
-// sphereLine.material.opacity = 0.25;
-// sphereLine.material.transparent = true;
+  gsap.fromTo(
+    sphereLine.rotation,
+    {
+      x: Math.PI * 0.05,
+    },
+    {
+      x: Math.PI * -0.05,
+      scrollTrigger: {
+        trigger: "#app",
+        start: "top top",
+        end: "+=1000",
+        scrub: 1.7
+      }
+    }
+  );
 
-scene.add(sphereLine)
+  scene.add(sphereLine);
 
-  scene.add(directionLight(.1, 0, 0, 100))
-  scene.add(directionLight(.9, 0, 80, 30))
-  scene.add(directionLight(.2, 0, -80, 60))
-  scene.add(directionLight(.3, -120, -120, -1))
-  scene.add(directionLight(.3, 120, -120, -1))
+  scene.add(directionLight(0.1, 0, 0, 100));
+  scene.add(directionLight(0.9, 0, 80, 30));
+  scene.add(directionLight(0.2, 0, -80, 60));
+  scene.add(directionLight(0.3, -120, -120, -1));
+  scene.add(directionLight(0.3, 120, -120, -1));
 
   app.addEventListener("click", () => {
     console.log("Test");
   });
 
-  const tl = gsap.timeline({ defaults: { ease: "power3.inOut" }, scrollTrigger: {
-    trigger: "#app",
-    pin: true,
-    start: "top top",
-    end: "+=2000",
-    scrub: 1
-  } });
-  tl.to(cube.position,
-    {
-      x: 3,
-      y: 3,
+  const tl = gsap.timeline({
+    defaults: { ease: "power3.inOut" },
+    scrollTrigger: {
+      trigger: "#app",
+      pin: true,
+      start: "top top",
+      end: "+=2000",
+      scrub: 1,
     },
-  ).to(cube.rotation,
+  });
+  tl.to(cube.position, {
+    x: 3,
+    y: 3,
+  }).to(
+    cube.rotation,
     {
       duration: 3,
       z: Math.PI * 1.25,
-      x: Math.PI * 0.75
+      x: Math.PI * 0.75,
     },
     "<"
   );
@@ -95,17 +118,14 @@ scene.add(sphereLine)
   }
 
   render();
-  
 
   const resize = () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
   };
-  
-  
+
   window.addEventListener("resize", () => {
     resize();
   });
 }
-
